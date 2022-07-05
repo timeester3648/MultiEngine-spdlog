e6acd9cb8bebacfe35c484eda2c1e0c558418458
timeester3648 <45339455+timeester3648@users.noreply.github.com>
2022-07-05 15:03:45 +0200

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,47 @@
+include "../../premake/common_premake_defines.lua"
+
+project "spdlog"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++latest"
+	cdialect "C17"
+	targetname "%{prj.name}"
+	inlining "Auto"
+
+	files {
+		"./**.h",
+		"./**.cpp"
+	}
+
+	includedirs {
+		"%{prj.name}/include",
+		
+		"%{IncludeDir.fmt}",
+		"%{IncludeDir.glfw}",
+		"%{IncludeDir.spdlog}",
+		"%{IncludeDir.vulkan_sdk}",
+		"%{IncludeDir.general_includes}"
+	}
+
+	defines {
+		"SPDLOG_COMPILED_LIB"
+	}
+
+ 	filter "configurations:Debug"
+		defines { "MLE_DEBUG_BUILD", "DEBUG" }
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
+
+	filter "configurations:Distribution"
+		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
