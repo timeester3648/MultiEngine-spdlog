4506ede6317ea382b27edc6daf0b5982021d0699
timeester3648 <weteringt@gmail.com>
2024-01-25 11:59:33 +0100

    Update

--- premake5.lua
+++ premake5.lua
@@ -30,22 +30,3 @@ project "spdlog"
 	defines {
 		"SPDLOG_COMPILED_LIB"
 	}
-
- 	filter "configurations:Debug"
-		defines { "MLE_DEBUG_BUILD", "DEBUG" }
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
-
-	filter "configurations:Distribution"
-		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
